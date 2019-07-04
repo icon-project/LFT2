56629f81e18bd7471f530c334eaaff689a28f790
yourhrh <asd236678@gmail.com>
2019-07-04 18:42:23 +0900

    Remove empty line

--- lft/consensus/term/rotate_term.py
+++ lft/consensus/term/rotate_term.py
@@ -20,7 +20,6 @@ from lft.consensus.term import Term
 
 
 class RotateTerm(Term):
-
     def __init__(self, num: int, validators: List[bytes]):
         self._num = num
         self.validators = validators
