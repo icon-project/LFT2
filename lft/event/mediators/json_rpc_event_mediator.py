50a063b92e07348e1febe82da4deeb502cdb3a36
Daehee Kim <zsaladinz@gmail.com>
2019-07-08 15:36:19 +0900

    [LFT-14] Make JsonRPC event log more readable

--- lft/event/mediators/json_rpc_event_mediator.py
+++ lft/event/mediators/json_rpc_event_mediator.py
@@ -1,10 +1,11 @@
 import aiohttp
 import base64
+import json
 import os
 import pickle
 from jsonrpcclient.clients.http_client import HTTPClient
 from jsonrpcclient.clients.aiohttp_client import AiohttpClient
-from typing import IO
+from typing import IO, Union
 from lft.event import EventMediator, EventInstantMediatorExecutor, EventRecorder, EventReplayer
 from lft.event import EventReplayerMediatorExecutor, EventRecorderMediatorExecutor
 
@@ -22,97 +23,83 @@ class JsonRpcEventRecorderMediatorExecutor(EventRecorderMediatorExecutor):
         super().__init__(event_recorder)
         self._io = io
         self._number = 0
-        self._started = False
 
     def execute(self, url: str, method: str, params: dict=None):
-        result = ""
+        result = None
         try:
             result = request(url, method, params)
         except Exception as e:
             result = e
+            raise e
+        else:
+            return result
         finally:
             self._write(result)
-            if isinstance(result, Exception):
-                raise result
-            else:
-                return result
 
     async def execute_async(self, url: str, method: str, params: dict=None):
-        result = ""
+        result = None
         try:
             result = await request_async(url, method, params)
         except Exception as e:
             result = e
-        finally:
-            self._write(result)
-            if isinstance(result, Exception):
-                raise result
-            else:
-                return result
-
-    def _write(self, result):
-        if self._number != self._event_recorder.number:
-            self._number = self._event_recorder.number
-
-            if self._started:
-                self._io.write(os.linesep)
-            self._io.write("#")
-            self._io.write(str(self._number))
-            self._io.write(os.linesep)
-        self._io.write("$")
-        dumped = base64.encodebytes(pickle.dumps(result)).decode()
-        dumped_len = len(dumped)
-        self._io.write(str(dumped_len))
-        self._io.write(os.linesep)
-        self._io.write(dumped)
-        self._io.write(os.linesep)
-        self._started = True
-
-        if isinstance(result, Exception):
-            raise result
+            raise e
         else:
             return result
+        finally:
+            self._write(result)
+
+    def _write(self, result):
+        number = self._event_recorder.number
+        serialized = serialize(number, result)
+        dumped = json.dumps(serialized)
+
+        self._io.write(dumped)
+        self._io.write(os.linesep)
 
 
 class JsonRpcEventReplayerMediatorExecutor(EventReplayerMediatorExecutor):
     def __init__(self, event_replayer: EventReplayer, io: IO):
         super().__init__(event_replayer)
         self._io = io
-        self._number = 0
 
     def execute(self, url: str, method: str, params: dict=None):
-        number = self._number
-        while number < self._event_replayer.number:
-            line = self._io.readline()
-            if line == os.linesep:
-                continue
-            if line[0] != "#":
-                continue
-            number = int(line[1:])
+        last_number = None
+        last_result = None
 
-        if number != self._event_replayer.number:
+        while self._is_less_last_number(last_number):
+            last_number, last_result = self._read()
+
+        if not self._is_equal_last_number(last_number):
             raise RuntimeError
 
-        self._number = number
-        while True:
-            line = self._io.readline()
-            if line != os.linesep:
-                break
-        if line[0] != "$":
-            raise RuntimeError
-
-        pickled_len = int(line[1:])
-        response_pickled = self._io.read(pickled_len)
-        response_pickled = response_pickled.encode()
-        response_pickled = base64.decodebytes(response_pickled)
-        result = pickle.loads(response_pickled)
-        if isinstance(result, Exception):
-            raise result
+        if isinstance(last_result, Exception):
+            raise last_result
         else:
-            return result
+            return last_result
 
     async def execute_async(self, url: str, method: str, params: dict=None):
         return self.execute(url, method, params)
+
+    def _read(self):
+        dumped = self._io.readline()
+        if not dumped:
+            return None, None
+        if dumped == os.linesep:
+            return None, None
+        serialized = json.loads(dumped)
+        return deserialize(serialized)
+
+    def _is_less_last_number(self, last_number: int):
+        return (
+            last_number is None or
+            last_number < self._event_replayer.number
+        )
+
+    def _is_equal_last_number(self, last_number: int):
+        return (
+            last_number is not None and
+            last_number == self._event_replayer.number
+        )
 
 
 class JsonRpcEventMediator(EventMediator):
@@ -131,7 +118,12 @@ def request(url: str, method: str, params: dict=None):
     if not params:
         params = {}
     client = HTTPClient(url)
-    return client.request(method, **params)
+    try:
+        response = client.request(method, **params)
+    except:
+        raise
+    else:
+        return response.data.result
 
 
 async def request_async(url: str, method: str, params: dict=None):
@@ -140,5 +132,41 @@ async def request_async(url: str, method: str, params: dict=None):
 
     async with aiohttp.ClientSession() as session:
         client = AiohttpClient(session, url)
-        response = await client.request(method, **params)
-        return response.data.result
+        try:
+            response = await client.request(method, **params)
+        except:
+            raise
+        else:
+            return response.data.result
+
+
+def serialize(number, result: Union[dict, Exception]) -> dict:
+    if isinstance(result, dict):
+        return {
+            "number": number,
+            "type": "dict",
+            "contents": result
+        }
+    if isinstance(result, Exception):
+        pickle_dumped = pickle.dumps(result)
+        base64_encoded = base64.encodebytes(pickle_dumped)
+        utf8_decoded = base64_encoded.decode()
+        return {
+            "number": number,
+            "type": "exception",
+            "contents": utf8_decoded
+        }
+    raise TypeError(f"Undefined result type to serialize. Type({type(result)})")
+
+
+def deserialize(serialized: dict) -> (int, Union[dict, Exception]):
+    type_ = serialized["type"]
+    if type_ == "dict":
+        return serialized["number"], serialized["contents"]
+    if type_ == "exception":
+        utf8_decoded: str = serialized["contents"]
+        base64_encoded = utf8_decoded.encode()
+        pickle_dumped = base64.decodebytes(base64_encoded)
+        return serialized["number"], pickle.loads(pickle_dumped)
+    raise TypeError(f"Undefined result type to deserialize. Type({type_})")
+
