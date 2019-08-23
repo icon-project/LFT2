c698287dd6a1b415dfac2bcbffa7b56744f96244
yourhrh <asd236678@gmail.com>
2019-08-23 12:16:48 +0900

    [LFT-11] Adjust ConsensusVotes in VoteCounter

--- lft/consensus/vote_counter.py
+++ lft/consensus/vote_counter.py
@@ -16,13 +16,13 @@
 from collections import defaultdict
 from typing import Dict, Set, Sequence
 
+from lft.app.data.consensus_votes import ConsensusVotes
 from lft.consensus.data import ConsensusVote, ConsensusData
 
 
 class VoteCounter:
     def __init__(self):
-        self._votes: Dict[bytes, Dict[bytes, ConsensusVote]] = defaultdict(lambda: {})
-        self._vote_counts: Dict[bytes, int] = defaultdict(lambda: 0)
+        self._votes: Dict[bytes, 'ConsensusVotes'] = PramsDefaultDict(ConsensusVotes)
         self._voters: Set[bytes] = set()
         self._majority_id: bytes = b''
 
@@ -31,32 +31,39 @@ class VoteCounter:
         return self._majority_id
 
     @property
-    def majority_votes(self) -> Sequence[ConsensusVote]:
-        return list(self._votes[self._majority_id].values())
+    def majority_votes(self) -> Sequence['ConsensusVote']:
+        return self._votes[self._majority_id].votes
 
     @property
     def majority_counts(self) -> int:
-        return self._vote_counts[self._majority_id]
+        return len(self._votes[self._majority_id])
 
     @property
     def voter_counts(self) -> int:
         return len(self._voters)
 
     @property
-    def all_votes(self) -> Sequence[ConsensusVote]:
+    def all_votes(self) -> Sequence['ConsensusVote']:
         all_votes = []
-        for votes in self._votes.values():
-            all_votes.extend(votes.values())
+        for votes_by_data_id in self._votes.values():
+            all_votes.extend(votes_by_data_id.votes)
         return all_votes
 
     def add_vote(self, vote: ConsensusVote):
         if not vote.is_not():
-            self._votes[vote.data_id][vote.voter_id] = vote
-            self._vote_counts[vote.data_id] += 1
+            self._votes[vote.data_id].add_vote(vote)
         self._voters.add(vote.voter_id)
+        self._update_majority(vote.data_id)
 
-        self._check_majority_is_change(vote.data_id)
-
-    def _check_majority_is_change(self, data_id: bytes):
-        if self._vote_counts[data_id] > self.majority_counts:
+    def _update_majority(self, data_id: bytes):
+        if len(self._votes[data_id]) > self.majority_counts:
             self._majority_id = data_id
+
+
+class PramsDefaultDict(defaultdict):
+    def __missing__(self, key):
+        if self.default_factory is None:
+            raise KeyError(key)
+        else:
+            ret = self[key] = self.default_factory(key)
+            return ret
