c0fa0d82191d7df4757cb26d88e25cc304a75838
yourhrh <asd236678@gmail.com>
2019-12-03 17:44:38 +0900

    [LFT-101] Writing test

--- tests/consensus/route_message.py
+++ tests/consensus/route_message.py
@@ -15,7 +15,10 @@ async def test_route_message_to_round():
         prev_id = b'id' + bytes([i-1])
         commit_id = b'id' + bytes([i-2])
 
-        prev_votes
+        if i == 0:
+            prev_votes = [vote_factory.create_vote(prev_id, genesis_data.id, 0, 0) for vote_factory in vote_factories]
+        else:
+            prev_votes = [vote_factory.create_vote(prev_id, commit_id, 1, i-1) for vote_factory in vote_factories]
         consensus.receive_data(DefaultData(
             id_=data_id,
             prev_id=prev_id,
@@ -23,10 +26,14 @@ async def test_route_message_to_round():
             number=i+1,
             term_num=1,
             round_num=i,
-            prev_votes=[vote_factory.create_vote(prev_id, commit_id, term_num=1, ) for vote_factory in vote_factories]
+            prev_votes=prev_votes
         ))
-        for vote in [vote_factory.create_vote(data_id) for vote_factory in vote_factories]:
+        for vote in [vote_factory.create_vote(data_id, prev_id, 1, i) for vote_factory in vote_factories]:
             consensus.receive_vote(vote)
+
+    # THEN
+    for i in range(0, 40, 4):
+        consensus._new_or_get_round(1, i)
 
     # THEN
 
