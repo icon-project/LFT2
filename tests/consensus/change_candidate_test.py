b65b7c5068d706df5dad22830fb1f92b1332c7b7
yourhrh <asd236678@gmail.com>
2019-12-06 18:14:23 +0900

    [LFT-101] Listing test methods

---
+++ tests/consensus/change_candidate_test.py
@@ -0,0 +1,33 @@
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_candidate_change_by_vote():
+    """ GIVEN a mock_round
+    WHEN Call try_candidate_change with
+    """
+
+
+@pytest.mark.asyncio
+async def test_candidate_change_by_data():
+    pass
+
+
+@pytest.mark.asyncio
+async def test_candidate_change_data_data():
+    pass
+
+
+@pytest.mark.asyncio
+async def test_candidate_change_vote_vote():
+    pass
+
+
+@pytest.mark.asyncio
+async def test_candidate_change_data_vote():
+    pass
+
+
+@pytest.mark.asyncio
+async def test_candidate_change_vote_data():
+    pass
